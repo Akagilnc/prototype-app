3719b88f79ff340512c84c6eb707b4cde825d047
DeronLee <akagilnc@gmail.com>
2016-05-10 23:09:46 +0800

    add questions list. But not really finished.

--- app/question-list.component.ts
+++ app/question-list.component.ts
@@ -7,6 +7,23 @@ import {ProgressService} from './services/progress.service';
   selector: 'question-list',
   template: `
   <ul class="list-group">
+    <div class="list-group">
+      <a href="#" class="list-group-item active">
+        
+        Questions 1 
+        <span class="glyphicon glyphicon-ok"></span>
+        </a>
+      
+      <a href="#" class="list-group-item"> 
+        Questions 2
+        <span class="glyphicon glyphicon-remove"></span>
+      
+      </a>
+      <a href="#" class="list-group-item"> Questions 3</a>
+      <a href="#" class="list-group-item"> Questions 4</a>
+      <a href="#" class="list-group-item"> Questions 5</a>
+    
+    </div>
   </ul>
   `,
   directives: [NgFor, NgClass]
