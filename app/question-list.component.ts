eb140ccf236466ccb5693593ef62e6c3a7fe12cb
DeronLee <akagilnc@gmail.com>
2016-05-11 10:42:44 +0800

    active worked fine

--- app/question-list.component.ts
+++ app/question-list.component.ts
@@ -1,34 +1,15 @@
-'use strict'
-
 import {Component, Input} from '@angular/core';
 import {NgFor, NgClass} from '@angular/common';
 import {IQuestion} from './all.interface.ts';
 import {ProgressService} from './services/progress.service';
-import {DataService} from "./services/data.service";
-
-var dataService = require("./services/data.service.ts")
-
 
 @Component({
   selector: 'question-list',
   template: `
   <ul class="list-group">
     <div class="list-group">
-     <!-- <a href="#" class="list-group-item active">
-        
-        Questions 1 
-        <span class="glyphicon glyphicon-ok"></span>
-        </a>
-      
-      <a href="#" class="list-group-item"> 
-        Questions 2
-        <span class="glyphicon glyphicon-remove"></span>
-      
-      </a>
-      <a href="#" class="list-group-item"> Questions 3</a>
-      <a href="#" class="list-group-item"> Questions 4</a>
-      <a href="#" class="list-group-item"> Questions 5</a> !-->
-      <a href="#" class="list-group-item" *ngFor="let displayQuestion of questionList">
+      <a href="#" class="list-group-item" *ngFor="let displayQuestion of questions"
+[ngClass]="{active: displayQuestion == activeQuestion}">
       {{displayQuestion.title}}</a>
     
     </div>
@@ -37,21 +18,14 @@ var dataService = require("./services/data.service.ts")
   directives: [NgFor, NgClass]
 })
 export class QuestionListComponent {
-  @Input('question') question: IQuestion
-  public questionList = this.progressService.getAllQuestions()
-
-
+  @Input('questions') questions: IQuestion
+  @Input('active-question') activeQuestion: IQuestion
 
   constructor(private progressService: ProgressService) {
-
+    this.setActiveQuestion();
   }
 
-  setActiveQuestion(question: IQuestion) {
-    var activeQuestion = this.progressService.getActiveQuestion()
-
-    question = this.questionList.find(activeQuestion[0].title);
-
-
+  setActiveQuestion() {
 
   }
 
